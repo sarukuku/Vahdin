bb5637fdba7b2423e7e419e22e1700f73ca54dd2
Roope Merikukka <roope.merikukka@gmail.com>
2013-02-22 12:07:09 +0200

    changed layout locators

--- src/vahdin/view/MarksSubview.java
+++ src/vahdin/view/MarksSubview.java
@@ -17,7 +17,7 @@ public class MarksSubview extends Subview {
 
     public MarksSubview() {
 
-        CustomLayout marksList = new CustomLayout("marks-list");
+        CustomLayout marksList = new CustomLayout("marks-sidebar");
         VerticalLayout tmp = new VerticalLayout();
 
         ArrayList<Mark> marks = new ArrayList<Mark>();
@@ -28,7 +28,7 @@ public class MarksSubview extends Subview {
         marks.add(m2);
 
         for (int i = 0; i < marks.size(); i++) {
-            CustomLayout layout = new CustomLayout("mark");
+            CustomLayout layout = new CustomLayout("mark-row");
 
             // Button to show BustsSubview with Busts under the clicked Mark
             Button title = new Button(marks.get(i).getTitle());
@@ -60,17 +60,17 @@ public class MarksSubview extends Subview {
 
             Label voteCount = new Label(marks.get(i).getVoteCount() + "");
 
-            layout.addComponent(voteUp, "mark-upvote-image");
-            layout.addComponent(voteCount, "mark-vote-count");
-            layout.addComponent(voteDown, "mark-downvote-image");
-            layout.addComponent(title, "mark-title-container");
+            layout.addComponent(voteUp, "mark-row-upvote-arrow");
+            layout.addComponent(voteCount, "mark-row-vote-count");
+            layout.addComponent(voteDown, "mark-row-downvote-arrow");
+            layout.addComponent(title, "mark-row-title");
 
             // FOR TESTING ONLY
             System.out.println(i + " " + marks.get(i).getTitle());
 
             tmp.addComponent(layout);
         }
-        marksList.addComponent(tmp, "marks-list-scrollable-area");
+        marksList.addComponent(tmp, "marks-list");
         setCompositionRoot(marksList);
 
     }
