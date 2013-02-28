a653feb337615a4efc82dadef0e49dd9bc7a9ba3
Juho Nurminen <juhonurm@gmail.com>
2013-02-28 15:47:47 +0200

    Google Maps integration.

--- src/vahdin/component/GoogleMap.java
+++ src/vahdin/component/GoogleMap.java
@@ -8,11 +8,49 @@ import com.vaadin.ui.JavaScript;
 @com.vaadin.annotations.JavaScript("js/google_map.js")
 public class GoogleMap extends CustomComponent {
 
+    private final String id;
+
+    /** Constructs a new GoogleMap with the specified API key. */
     public GoogleMap(String apiKey) {
-        String id = UUID.randomUUID().toString();
+        id = UUID.randomUUID().toString();
         setId(id);
 
         JavaScript.getCurrent().execute(
-                "new GoogleMap('" + id + "', '" + apiKey + "');");
+                "window['" + id + "'] = new GoogleMap('" + id + "', '" + apiKey
+                        + "');");
+    }
+
+    /**
+     * Adds a marker at the given coordinates.
+     * 
+     * @return The created Marker object.
+     */
+    public Marker addMarker(double lat, double lng) {
+        String markerId = UUID.randomUUID().toString();
+        JavaScript.getCurrent().execute(
+                "window['" + id + "'].addMarker(" + lat + "," + lng + ",'"
+                        + markerId + "');");
+        return new Marker(lat, lng, markerId);
+    }
+
+    /** Removes the specified marker. */
+    public void removeMarker(Marker marker) {
+        JavaScript.getCurrent().execute(
+                "window['" + id + "'].removeMarker('" + marker.id + "')");
+    }
+
+    public class Marker {
+
+        public final double latitude;
+        public final double longitude;
+
+        private final String id;
+
+        /** Constructs a new marker with the given coordinates. */
+        private Marker(double lat, double lng, String id) {
+            latitude = lat;
+            longitude = lng;
+            this.id = id;
+        }
     }
 }
