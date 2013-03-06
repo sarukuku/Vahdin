1790c614e25783d634dd71562495ba10d7010b32
Roope Merikukka <roope.merikukka@gmail.com>
2013-03-07 01:07:27 +0200

    asf

--- src/vahdin/data/User.java
+++ src/vahdin/data/User.java
@@ -155,12 +155,15 @@ public class User implements Item {
         }
         return null;
     }
-    
-    public static Vote getVote(Bust bust) {
-    	final VahdinUI ui = (VahdinUI) UI.getCurrent();
-    	List<Vote> all = Vote.loadAll();
+
+    public Vote getVote(Bust bust) {
+        final VahdinUI ui = (VahdinUI) UI.getCurrent();
+        List<Vote> all = Vote.loadAll();
         for (int i = 0; i < all.size(); i++) {
-            if (all.get(i).getTargetItemId() == bust.getId() && all.get(i).getType().equals("Bust") && all.get(i).getUserId() == ui.getCurrentUser().getUserId()) {
+            if (all.get(i).getTargetItemId() == bust.getId()
+                    && all.get(i).getType().equals("Bust")
+                    && all.get(i).getUserId() == ui.getCurrentUser()
+                            .getUserId()) {
                 return all.get(i);
             }
         }
